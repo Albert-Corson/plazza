ab59bcfe5b6e6894916bc26e3e64e7ea47cb63ec
Albert Corson <albert.corson@epitech.eu>
2020-05-03 17:58:00 +0200

    [MOD] KitchenStatus: added << and >> operators

--- deps/KitchenStatus.hpp
+++ deps/KitchenStatus.hpp
@@ -10,26 +10,16 @@
 #include <regex>
 #include <tuple>
 
-#include "Exception.hpp"
 #include "pizzaSize.hpp"
 
 struct KitchenStatus {
-    class Exception : public ::Exception {
-    public:
-        Exception(const std::string &msg)
-            : ::Exception("KitchenStatus::Exception: " + msg)
-        {
-        }
-        ~Exception() override = default;
-    };
-
     size_t activeCooks = 0;
     size_t totalCooks = 0;
     size_t orderQueueCapacity = 0;
     std::vector<std::tuple<std::string, pizzaSize_t, bool>> orderQueue;
 
     // returns a string buffer containing the kitchen infos in the following format:
-    // <activeCooks> <totalCooks> <orderQueueCapacity>[:<pizza> <size> <status>]...
+    // <activeCooks> <totalCooks> <orderQueueCapacity> [<pizza> <size> <status>]...
     const std::string &serialize() const noexcept
     {
         static std::string buffer;
@@ -41,10 +31,10 @@ struct KitchenStatus {
         buffer += ' ';
         buffer += std::to_string(orderQueueCapacity);
         for (const auto &it : orderQueue) {
-            buffer += ':';
+            buffer += ' ';
             buffer += std::get<0>(it);
             buffer += ' ';
-            buffer += pizzaSizeStr.at(std::get<1>(it));
+            buffer += std::to_string(int(std::get<1>(it)));
             buffer += ' ';
             buffer += std::to_string(std::get<2>(it));
         }
@@ -52,29 +42,49 @@ struct KitchenStatus {
     }
 
     // parses a string buffer formatted the way of `dump()`'s output
-    void deserialize(const std::string &line)
+    bool deserialize(const std::string &line)
     {
-        std::regex format("^ *([0-9]+) +([0-9]+) +([0-9]+)");
+        std::regex format("^ *([0-9]+) +([0-9]+) +([0-9]+) *");
         std::smatch matches;
 
         if (!std::regex_search(line, matches, format))
-            throw Exception("deserialize: invalid format");
+            return (false);
         try {
             activeCooks = std::stoul(matches.str(1));
             totalCooks = std::stoul(matches.str(2));
             orderQueueCapacity = std::stoul(matches.str(3));
 
-            format = "^ *([A-z0-9_-]+) +([0-5]) +([01])";
-            std::string buffer = line;
-            for (size_t pos = buffer.find(':'); pos != std::string::npos; pos = buffer.find(':')) {
-                buffer = buffer.substr(pos);
+            format = "^ *([A-z0-9_-]+) +([0-5]) +([01]) *";
+            std::string buffer = matches.suffix();
+            while (buffer.size()) {
                 if (!std::regex_search(buffer, matches, format))
-                    throw Exception("deserialize: invalid format");
+                    return (false);
                 orderQueue.emplace_back(matches.str(1), pizzaSize_t(std::stoi(matches.str(2))), std::stoi(matches.str(3)));
+                buffer = matches.suffix();
             }
         } catch (...) {
-            throw Exception("deserialize: invalid format");
+            return (false);
         }
+        return (true);
+    }
+
+    // parses a string vector formatted the way of `dump()`'s output
+    // ie: {<activeCooks>, <totalCooks>, <orderQueueCapacity>, [<pizza>, <size>, <status>,]...}
+    bool deserialize(const std::vector<std::string> &words, size_t offset = 0)
+    {
+        try {
+            activeCooks = std::stoul(words[offset + 0]);
+            totalCooks = std::stoul(words[offset + 1]);
+            orderQueueCapacity = std::stoul(words[offset + 2]);
+
+            const auto end = words.cend();
+            for (auto it = words.cbegin() + offset + 3; it != end; it += 3) {
+                orderQueue.emplace_back(*it, pizzaSize_t(std::stoi(*(it + 1))), std::stoi(*(it + 2)));
+            }
+        } catch (...) {
+            return (false);
+        }
+        return (true);
     }
 
     // dumps infos in a human-readable format
@@ -98,3 +108,27 @@ struct KitchenStatus {
         }
     }
 };
+
+std::string &operator<<(std::string &lhs, const KitchenStatus &rhs)
+{
+    lhs = rhs.serialize();
+    return (lhs);
+}
+
+std::ostream &operator<<(std::ostream &lhs, const KitchenStatus &rhs)
+{
+    lhs << rhs.serialize();
+    return (lhs);
+}
+
+KitchenStatus &operator>>(const std::vector<std::string> &lhs, KitchenStatus &rhs)
+{
+    rhs.deserialize(lhs);
+    return (rhs);
+}
+
+KitchenStatus &operator>>(const std::string &lhs, KitchenStatus &rhs)
+{
+    rhs.deserialize(lhs);
+    return (rhs);
+}
