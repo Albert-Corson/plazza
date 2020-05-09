db2323f21fea784f77f6d26fa782ac63f4ca0ae3
35Master <arthur.viard@epitech.eu>
2020-05-09 19:29:43 +0200

    [ADD] Network.hpp (inter-process communication through sockets)

---
+++ deps/IPC/Network.hpp
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2020
+** Visual Studio Live Share (Workspace)
+** File description:
+** Network
+*/
+
+#pragma once
+
+#include "IIPC.hpp"
+#include "Socket.hpp"
+
+// Inter-process communication through sockets
+class Network : public IIPC
+{
+public:
+    Network(){};
+    Network(const Network &other){};
+    ~Network(){};
+
+    Network &operator=(const Network &other)
+    {
+        return (*this);
+    }
+
+    // read message from std::cin
+    void receive(char *buffer, std::streamsize size) override final
+    {
+        _socket.read(buffer, size);
+    }
+    // read next line from std::cin
+    bool getline(std::string &buffer) override final
+    {
+        if (!_socket.getline(buffer))
+            return (false);
+        return (true);
+    }
+    // write message to std::cout
+    void send(const char *buffer, std::streamsize size) override final
+    {
+        _socket.write(buffer, size);
+    }
+    bool good() const override final
+    {
+        return (_socket.good() && _socket.good());
+    }
+
+private:
+    Socket _socket;
+};
